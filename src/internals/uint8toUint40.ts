9c9beda98ca8cba14c8122e1351d25040671c9fe
Ege Özkan <egeemirozkan24@gmail.com>
2026-07-26 15:58:07 +0300

    chore: add license preamble to files

--- src/internals/uint8toUint40.ts
+++ src/internals/uint8toUint40.ts
@@ -1,4 +1,22 @@
 /**
+@ambertide/basehieroglyph
+Copyright (C) 2026  Ege Özkan
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/**
  * Convert a uint8[] to uint40[] where the memory
  * is considered contigious, for instance:
  * input: [0xFF, 0xAA, 0XBB, 0xCC, 0xDD, 0xCC, 0xDD, 0AA]
