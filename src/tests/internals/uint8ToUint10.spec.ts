c03c6c68a594292befe474f3fb22e196304b97b1
Ege Özkan <egeemirozkan24@gmail.com>
2026-08-02 17:02:45 +0300

    test: add complex test cases to uint8toUint10 conversions

--- src/tests/internals/uint8ToUint10.spec.ts
+++ src/tests/internals/uint8ToUint10.spec.ts
@@ -20,7 +20,12 @@ import {expect, test} from '@jest/globals';
 
 import { uint8toUint10 } from '../../internals/uint8ToUint10';
 
-test('Check conversion from 8 bit unsigned array to 10 bit unsigned array', () => {
-  const testCase = new Uint8Array([0xAA, 0xBB, 0xCC]);
-  expect(uint8toUint10(testCase)).toEqual([0x2AA, 0x3BC, 0xC]);
+test.each([
+  [[0xAA, 0xBB, 0xCC, 0xDD, 0xEE, 0xFF], [0x2AA, 0x3BC, 0x337, 0x1EE, 0xFF]],
+  [[0xAA, 0xBB, 0xCC], [0x2AA, 0x3BC, 0xC]],
+  [[0xBB, 0xCC], [0x2EF, 0x0C]],
+  [[0xCC], [0xCC]]
+])('uint8[] (%s) -> uint10[] (%s)', (input, expected) => {
+  const testCase = new Uint8Array(input);
+  expect(uint8toUint10(testCase)).toEqual(expected);
 });
