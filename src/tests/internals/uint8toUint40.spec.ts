decad5e090e9b1f43cb8ad068e40d3543d4f6bd6
Ege Özkan <egeemirozkan24@gmail.com>
2026-08-02 17:02:24 +0300

    test: add test cases for uint8toUint40 conversions

--- src/tests/internals/uint8toUint40.spec.ts
+++ src/tests/internals/uint8toUint40.spec.ts
@@ -20,7 +20,11 @@ import {expect, test} from '@jest/globals';
 
 import { uint8toUint40 } from '../../internals/uint8toUint40';
 
-test('Check conversion from 8 bit unsigned array to 40 bit unsigned array', () => {
-  const testCase = new Uint8Array([0xAA, 0xBB, 0xCC, 0xDD, 0xFF, 0x11, 0x22, 0x33]);
-  expect(uint8toUint40(testCase)).toEqual([0xAABBCCDDFF, 0x112233]);
+test.each([
+  [[0xAA, 0xBB, 0xCC, 0xDD, 0xEE, 0xFF], [0xAABBCCDDEE, 0xFF]],
+  [[0xAA, 0xBB, 0xCC, 0xDD, 0xFF, 0x11, 0x22, 0x33], [0xAABBCCDDFF, 0x112233]],
+  [[0xAA, 0xBB], [0xAABB]]
+])('uint8[] (%s) -> uint40[] (%s)', (input, expected) => {
+  const testCase = new Uint8Array(input);
+  expect(uint8toUint40(testCase)).toEqual(expected);
 });
