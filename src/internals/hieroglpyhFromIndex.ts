919e251623e64148b5ec9e6b2050b8c4e9afd570
Ege Özkan <egeemirozkan24@gmail.com>
2026-08-02 17:25:03 +0300

    feat: add to hieroglyph conversions

---
+++ src/internals/hieroglpyhFromIndex.ts
@@ -0,0 +1,32 @@
+/**
+@ambertide/basehieroglyph
+Copyright (C) 2026  Ege Özkan
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+import { HieroglyphIndexError } from './HieroglpyhIndexError';
+
+/**
+ * Return a hieroglpyh given its index.
+ * 
+ * @param index Index of the hieroglpyh... glyph.
+ * @return the hieroglpyh string.
+ */
+export const hieroglpyhFromIndex = (index: number): string => {
+  if (index < 0 || index > 0x3FF || !Number.isInteger(index)) {
+    throw new HieroglyphIndexError(`Index ${index} is out of allowed range [0x00, 0x3FF]`);
+  }
+  return String.fromCodePoint(0x13000 + index);
+};
---
+++ src/tests/internals/hieroglpyhFromIndex.spec.ts
@@ -0,0 +1,37 @@
+/**
+@ambertide/basehieroglyph
+Copyright (C) 2026  Ege Özkan
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+import {expect, test} from '@jest/globals';
+
+import { hieroglpyhFromIndex } from '../../internals/hieroglpyhFromIndex';
+import { HieroglyphIndexError } from '../../internals/HieroglpyhIndexError';
+
+test.each([
+  [0, '𓀀'],
+  [0x3FF, '𓏿']
+])('hieroglpyhFromIndex(%i) = %s', (index, hieroglyph) => {
+  expect(hieroglpyhFromIndex(index)).toBe(hieroglyph);
+});
+
+test.each([
+  -1,
+  0x3FF + 1,
+  0.4
+])('hieroglpyhFromIndex(%i) -> Exception', index => {
+  expect(() => hieroglpyhFromIndex(index)).toThrow(HieroglyphIndexError);
+});
